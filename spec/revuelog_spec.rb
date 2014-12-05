61f38cafb66d0a3848e6cbcea837f8eb8ed30939
Benoit <benoit@hopsandfork.com>
2014-12-05 19:50:16 +0100

    Clean rspec tests
    With be_kind_of and to_eq of each value of each key.

--- spec/revuelog_spec.rb
+++ spec/revuelog_spec.rb
@@ -9,7 +9,10 @@ describe "create object from irc message" do
   end
 
   describe "checking hash object" do
-    it { expect(revueobj.to_hash).to eq("time" => "2014-11-28 13:58:33 +0100", "nick" => "nickname", "message" => "Love TDD") }
+    it { expect(revueobj.to_hash).to be_kind_of(Hash)}
+    it { expect(revueobj.to_hash["time"]).to eq("2014-11-28 13:58:33 +0100")}
+    it { expect(revueobj.to_hash["nick"]).to eq("nickname")}
+    it { expect(revueobj.to_hash["message"]).to eq("Love TDD")}
   end
 
   describe "insert data into database" do
