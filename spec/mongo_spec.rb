7476add57fdd4db57b9ad754052cd7e8988982db
Benoit <benoit@hopsandfork.com>
2014-12-08 12:43:42 +0100

    Quick code review of the Mongo spec
    Still need big refactoring

--- spec/mongo_spec.rb
+++ spec/mongo_spec.rb
@@ -5,8 +5,6 @@ include Mongo
 
 class MongodbSpec
 
-  attr_reader :key, :value, :revuehash
-
   def self.dbnew
     db = MongoClient.new("localhost", 27017).db("revue_db")
     @coll = db["revue-collection-rspec"]
@@ -14,31 +12,35 @@ class MongodbSpec
   end
 
   def self.find(key, value)
-    @key = key
-    @value = value
-    @coll.find(@key => @value).to_a
+    @coll.find({key => value}, :fields => [key]).to_a
+  end
+
+  def self.distinct(key)
+    @coll.distinct(key).to_a
   end
 
   def self.dbinsert(revuehash)
-    @revuehash = revuehash
-    @coll.insert(@revuehash)
+    @coll.insert(revuehash)
   end
 
   def self.dbclean
-    @coll.drop_collection['revue-collection-rspec']
+    @coll.drop
   end
 end
 
 describe "Test mongo db" do
   before(:each) do
-    revuehash = Revuelog.new("2014-11-28 13:58:33 +0100", "nickname", "Love TDD").to_hash
+    revuehash = Revuelog.new("2012-11-28 13:58:33 +0100", "Paul", "Love TDD and Paul").to_hash
     MongodbSpec.dbnew.dbinsert(revuehash)
+    revuehash2 = Revuelog.new("2014-11-28 16:58:33 +0100", "Semia", "Love TDD !!").to_hash
+    MongodbSpec.dbnew.dbinsert(revuehash2)
   end
 
   after(:each) do
-    MongodbSpec.dbclean
+    MongodbSpec.dbnew.dbclean
   end
 
-  it { expect(MongodbSpec.find("nick", "nickname")).to be_kind_off(Hash)}
+  it { expect(MongodbSpec.find("nick", "Paul").first['nick']).to eq('Paul')}
+  it { expect(MongodbSpec.distinct("nick")).to eq(['Paul','Semia'])}
 end
 
