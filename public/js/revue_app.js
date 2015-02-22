30de921c214aa013b46b65d04ee9ffc2760448d8
Benoit Tigeot <benoit@hopsandfork.com>
2015-02-22 21:51:22 +0100

    No working version

--- public/js/revue_app.js
+++ public/js/revue_app.js
@@ -25,6 +25,36 @@
     .error(function(data) {
       console.log("Error when getting the json at /most_active");
     });
+
   });
+  var chart_width = 500;
+  var chart_height = 250;
+
+
+  app.controller('mainController', ["$scope", function($scope){
+    $scope.chart_data = "40, 80, 15, 60, 23, 95";
+
+    $scope.updateChart = function() {
+      var index = 0;
+      var data = angular.fromJson("[" + $scope.chart_data + "]");
+      var bar_width = 500 / (data.length); 
+
+      /* Clear out the existing elements. */
+      d3.selectAll('.chart').selectAll('div').remove();
+
+      d3.select(".chart")
+          .selectAll("div")
+          .data(data)
+          .enter().append("div")
+          .style("width", function(d) { return bar_width + "px"; })
+          .style("height", function(d) { return d + "%"; })
+          .style("left", function(d) { return (index++) * (bar_width + 2) + "px"; }); /* +2 as we have a 1px margin*/
+    }
+
+    /* Show the chart on load. */
+    $scope.updateChart();
+  }]);
 
 })();
+
+
